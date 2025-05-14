d0d4e0e0b0e111447df651fc08d9214229b19500
Fernando Valle <jjonathanvalle@gmail.com>
2025-05-14 17:17:34 -0300

    combinamos html con JS para armar pequeñas apps

--- JavaScript/manipulacion-dom/productos.js
+++ JavaScript/manipulacion-dom/productos.js
@@ -4,11 +4,27 @@ const productos = [];
 
 function agregarProducto() {
     const input = document.getElementById("Producto");
-    const nombre = nombre.value.trim();
+    const nombre = input.value.trim();
     if (nombre === '') return;
 
     const li = document.createElement("li");
-    li.textContent = listaProductos;
-    li.onclick = () => li.remove();
-    productos.push({nombre, comprado: false});
+    li.textContent = nombre;
+    li.onclick = () => li.remove(); // Eliminar producto al hacer click
+    document.getElementById("listaProductos").append(li);  // Agregar a la lista visual
+    productos.push(producto);
 }
+
+const btnComprado = document.createElement("boton1");
+  btnComprado.textContent = "Comprado";
+  btnComprado.classList.add("comprado-btn1");
+  btnComprado.onclick = () => marcarComprado(li, producto);
+
+const btnEliminar = document.createElement("boton2");
+  btnEliminar.textContent = "Eliminar";
+  btnEliminar.classList.add("eliminar-btn2");
+  btnEliminar.onclick = () => eliminarProducto(li, producto);
+
+li.appendChild(btnComprado);
+li.appendChild(btnEliminar);
+document.getElementById("listaProductos").append(li);
+
